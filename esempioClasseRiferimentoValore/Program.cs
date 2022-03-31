d814bd4af507aec9dd9f27f03bd5a6b49f2e40c2
Andrea <Andrea@DESKTOP-BDOFMVL>
2022-03-31 08:17:01 +0200

    fix struttura codice

--- esempioClasseRiferimentoValore/Program.cs
+++ esempioClasseRiferimentoValore/Program.cs
@@ -1,75 +1,15 @@
-﻿namespace esempioClasseRiferimento
+﻿namespace Program
 {
-    class Uomo
-    {
-        public int età;
-        public float temperatura;
+	using esempioClasseRiferimento;
 
-        public Uomo()
-        {
-            età = 18;
-            temperatura = 38.5f;
-        }
-    }
+	class Program
+	{
+		public static void Main()
+		{
+			TipoRiferimento.dimostrazioneClasseRiferimento();
+			TipoRiferimento.dimostrazioneArrayRiferimento();
+		}
 
-    class MainProgram
-    {
-        public static void Main()
-        {
-            dimostrazioneClasseRiferimento();
-            dimostrazioneArrayRiferimento();
-        }
-
-        private static void dimostrazioneClasseRiferimento()
-        {
-            Console.WriteLine(Environment.NewLine);
-            Console.WriteLine("esempio di manipolazione di un'istanza di una classe assegnata a due variabili diverse");
-
-            
-            Uomo mrJekil = new Uomo();
-            Uomo mrHyde = mrJekil;
-
-            Console.WriteLine("Mr Jekil ha " + mrJekil.età + " e la sua temperatura corporea è " + mrJekil.temperatura); // output : età = 18, temperatura = 38.5f
-            Console.WriteLine("Mr Hyde ha " + mrHyde.età + " e la sua temperatura corporea è " + mrHyde.temperatura); // output : età = 18, temperatura = 38.5f
-            Console.WriteLine(Environment.NewLine);
-
-            // quando andiamo a inizializzare le due variabili mrJekil e mrHyde, assegnando a mrHyde la variabile mrJekil, non assegniamo il valore
-            // ma il riferimento all'istanza dell'oggetto Uomo creata con l'inizializzazione e assegnazione di mrJekil.
-            // Per questo motivo, possiamo vedere che quando andiamo a modificare il valore degli attributi dell'istanza della classe Uomo assegnata a mrJekil,
-            // anche i valori relativi agli attributi di mrHyde cambiano di conseguenza.
-
-            mrJekil.età = 20;
-            mrJekil.temperatura = 40.5f;
-
-            Console.WriteLine("Passano due anni...");
-            Console.WriteLine(Environment.NewLine);
-            Console.WriteLine("Mr Jekil ha " + mrJekil.età + " e la sua temperatura corporea è " + mrJekil.temperatura); // output : età = 20, temperatura = 40.5f
-            Console.WriteLine("Mr Hyde ha " + mrHyde.età + " e la sua temperatura corporea è " + mrHyde.temperatura); // output : età = 20, temperatura = 40.5f
-
-            // cambiando mrJekil, abbiamo cambiato pure mrHyde, questo poichè una classe è un tipo riferimento e non un tipo valore
-        }
-        private static void dimostrazioneArrayRiferimento()
-        {
-            Console.WriteLine(Environment.NewLine);
-            Console.WriteLine("esempio di manipolazione di un array stringa creato all'assegnazione di valore ad una stringa e poi assegnato ad una nuova variabile copia della prima");
-            Console.WriteLine(Environment.NewLine);
-            Console.WriteLine("Array prima della modifica : ");
-
-
-            String[] nomiArray = { "luca", "paolo", "michelle" };
-            String[] nuoviNomiArray = nomiArray;
-
-            Console.WriteLine(nomiArray[0]);
-            Console.WriteLine(nuoviNomiArray[0]);
-
-            nuoviNomiArray[0] = "dario";
-
-            Console.WriteLine(Environment.NewLine);
-            Console.WriteLine("Array dopo la modifica : ");
-            Console.WriteLine(nomiArray[0]);
-            Console.WriteLine(nuoviNomiArray[0]);
-        }
-       
-
-    }
+	}
 }
+
